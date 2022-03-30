ae62653d283f0338b61b409528bb51242806a9a1
Simone Benati <simone.benati@outlook.com>
2022-03-30 18:13:25 +0200

    re-thinkered the whole playbook and added an installer

---
+++ files/file-pkgs/installer.py
@@ -0,0 +1,6 @@
+import os
+
+
+os.system("dpkg -i /install-temp/file-pkgs/google-chrome-stable_current_amd64.deb")
+os.system("dpkg -i /install-temp/file-pkgs/code_1.65.2-1646927742_amd64.deb")
+os.system("dpkg -i /install-temp/file-pkgs/discord-0.0.17.deb")
