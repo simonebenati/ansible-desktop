8d6db2c202d489c9b7df8ffc1f6460d8b5e8944a
Simone Benati <simone.benati@outlook.com>
2022-03-30 15:10:06 -0700

    clean up after install

--- files/file-pkgs/installer.py
+++ files/file-pkgs/installer.py
@@ -1,6 +1,10 @@
 import os
 
-
+# packages to install
 os.system("dpkg -i /install-temp/file-pkgs/google-chrome-stable_current_amd64.deb")
 os.system("dpkg -i /install-temp/file-pkgs/code_1.65.2-1646927742_amd64.deb")
 os.system("dpkg -i /install-temp/file-pkgs/discord-0.0.17.deb")
+
+#post install
+os.system("rm -r /install-temp")
+os.system("history -c")
